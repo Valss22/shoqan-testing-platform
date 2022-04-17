8ade77d5acae93a409bec677b84f950ef09b022b
Valss22 <valsshokorov@gmail.com>
2022-04-17 23:40:43 +0600

    add response user profile

--- app/user_profile/service.py
+++ app/user_profile/service.py
@@ -13,19 +13,16 @@ import json
 
 class UserProfileService:
     async def write_to_profile(self, user_profile: UploadFile, photo, auth_header):
-        user_profile = user_profile.file.__dict__["_file"].read().decode("utf-8")
-
         uploaded_photo = cloudinary.uploader.upload(photo.file, resource_type="auto")
-
         current_user_id = get_current_user_id(auth_header)
-        user_profile = await UserProfile.create(
-            **json.loads(user_profile),
-            photo=uploaded_photo["secure_url"]
-        )
-
         user_obj = User.get(id=current_user_id)
 
         if user_obj:
+            user_profile = user_profile.file.__dict__["_file"].read().decode("utf-8")
+            user_profile = await UserProfile.create(
+                **json.loads(user_profile),
+                photo=uploaded_photo["secure_url"]
+            )
             await user_profile.save()
             await user_obj.update(user_profile=user_profile)
             return user_profile
