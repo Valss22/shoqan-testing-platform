cffbe1b35b2c24f10c741f2838f5d85a7c1ab0ac
Valss22 <valsshokorov@gmail.com>
2022-04-18 17:44:57 +0600

    fix post profile

--- app/user_profile/service.py
+++ app/user_profile/service.py
@@ -13,22 +13,16 @@ class UserProfileService:
     async def write_to_profile(self, user_profile: UploadFile, photo, auth_header):
         uploaded_photo = cloudinary.uploader.upload(photo.file, resource_type="auto")
         current_user_id = get_current_user_id(auth_header)
-        user_obj = User.get(id=current_user_id)
+        user_obj = await User.get(id=current_user_id)
 
-        if user_obj:
-            user_profile = user_profile.file.__dict__["_file"].read().decode("utf-8")
-            user_profile = await UserProfile.create(
-                **json.loads(user_profile),
-                photo=uploaded_photo["secure_url"]
-            )
-            await user_profile.save()
-            await user_obj.update(user_profile=user_profile)
-            return user_profile
-
-        return JSONResponse(
-            {"msg": "user doesnt exist"},
-            status.HTTP_400_BAD_REQUEST
+        user_profile = user_profile.file.__dict__["_file"].read().decode("utf-8")
+        user_profile = await UserProfile.create(
+            **json.loads(user_profile),
+            photo=uploaded_photo["secure_url"]
         )
+        await user_profile.save()
+        await user_obj.update(user_profile=user_profile)
+        return user_profile
 
     async def get_profile(self, auth_header: str):
         current_user_id = get_current_user_id(auth_header)
@@ -42,4 +36,3 @@ class UserProfileService:
             profile = profile.__dict__
 
         return profile
-
