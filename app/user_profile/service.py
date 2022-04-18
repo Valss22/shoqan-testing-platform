bdaf1c7796f2ecad8010ac6e5e7c393d72a5fb55
deger4beger <deger.begerrr@gmail.com>
2022-04-18 18:29:05 +0600

    save profile fixed

--- app/user_profile/service.py
+++ app/user_profile/service.py
@@ -13,6 +13,7 @@ class UserProfileService:
     async def write_to_profile(self, user_profile: UploadFile, photo, auth_header):
         uploaded_photo = cloudinary.uploader.upload(photo.file, resource_type="auto")
         current_user_id = get_current_user_id(auth_header)
+
         user_obj = await User.get(id=current_user_id)
 
         user_profile_str = user_profile.file.__dict__["_file"].read().decode("utf-8")
@@ -22,7 +23,8 @@ class UserProfileService:
             photo=uploaded_photo["secure_url"]
         )
         await user_profile_obj.save()
-        await user_obj.update(user_profile=user_profile_obj)
+        user_obj.user_profile = user_profile_obj
+        await user_obj.save(update_fields=["user_profile_id"])
         return user_profile_obj
 
     async def get_profile(self, auth_header: str):
