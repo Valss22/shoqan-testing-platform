ff85e4b046009fbb11f0f8645af21e859cd4feac
Valss22 <valsshokorov@gmail.com>
2022-04-19 22:53:13 +0600

    fix tests relation

--- src/discipline/model.py
+++ src/discipline/model.py
@@ -6,8 +6,4 @@ from src.discipline.types import Disciplines
 class Discipline(models.Model):
     id = fields.UUIDField(pk=True)
     name = fields.CharEnumField(Disciplines)
-    tests = fields.ForeignKeyField(
-        "models.Test",
-        on_delete="SET NULL",
-        null=True
-    )
+
--- src/test/model.py
+++ src/test/model.py
@@ -4,3 +4,8 @@ from tortoise import models, fields
 class Test(models.Model):
     id = fields.UUIDField(pk=True)
     file = fields.CharField(max_length=150)
+    disciplinies = fields.ForeignKeyField(
+        "models.Test",
+        on_delete="SET NULL",
+        null=True
+    )
