acee9d4d00a0d072c4e9ba512845c87b5242cc76
Valss22 <valsshokorov@gmail.com>
2022-05-12 00:03:22 +0600

    fix competence

--- src/competence/service.py
+++ src/competence/service.py
@@ -7,7 +7,8 @@ class CompetenceService:
     async def get_competencies(self, auth_header: str):
         user_id = get_current_user_id(auth_header)
         tests = await Test.filter(
-            user_to_tests__user_id=user_id
+            user_to_tests__user_id=user_id,
+            user_to_tests__passed=True,
         ).prefetch_related("competencies")
 
         competencies: list[str] = []
