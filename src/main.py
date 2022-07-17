e919316b03903c9442c15f766591445bff6cf4ed
Valss22 <valsshokorov@gmail.com>
2022-07-17 13:32:53 +0600

    Add heroku pdf

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 # type: ignore
-import subprocess
 
 import cloudinary
 from fastapi import FastAPI
@@ -35,12 +34,12 @@ cloudinary.config(
 
 register_tortoise(
     app,
-    db_url=f'postgres:'
-           f'//{os.getenv("USER")}:'
-           f'{os.getenv("PASSWORD")}@'
-           f'{os.getenv("HOST")}/'
-           f'{os.getenv("DATABASE")}',
-    # db_url=os.getenv("DATABASE_URL"),
+    # db_url=f'postgres:'
+    #        f'//{os.getenv("USER")}:'
+    #        f'{os.getenv("PASSWORD")}@'
+    #        f'{os.getenv("HOST")}/'
+    #        f'{os.getenv("DATABASE")}',
+    db_url=os.getenv("DATABASE_URL"),
 
     modules={"models": [
         "src.user.model",
